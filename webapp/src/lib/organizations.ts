dcd1be380d08b26e8fc270fb6d9e3b78ea61d502
Sven Fibelkorn <sven.fibelkorn@gmail.com>
2026-02-20 09:35:32 +0100

    fix duplicate grafana ports

--- webapp/src/lib/organizations.ts
+++ webapp/src/lib/organizations.ts
@@ -20,6 +20,32 @@ export const ORGANIZATION_FILTERS: OrganizationFilterOption[] = [
     slug: 'die-zeit',
     name: 'Die Zeit',
   },
+  {
+    label: 'Foreign Affairs',
+    slug: 'foreign-affairs',
+    name: 'Foreign Affairs',
+  },
+  {
+    label: 'Semafor',
+    slug: 'semafor',
+    name: 'Semafor',
+  },
+  {
+    label: 'Ars Technica',
+    slug: 'ars-technica',
+    name: 'Ars Technica',
+  },
+  {
+    label: 'FAZ',
+    slug: 'faz',
+    name: 'FAZ',
+  },
+  {
+    label: 'Deutsche Welle',
+    slug: 'dw',
+    name: 'Deutsche Welle',
+  },
+  
 ]
 
 const slugToNameMap = new Map(ORGANIZATION_FILTERS.map((item) => [item.slug, item.name]))
